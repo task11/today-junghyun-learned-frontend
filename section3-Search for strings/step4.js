09f98f2b9e25ec4d1839bfac8444553226236959
task11 <task11@naver.com>
2021-12-03 00:18:54 +0900

    step4 - 가장 짧은 문자거리

--- section3-Search for strings/step4.js
+++ section3-Search for strings/step4.js
@@ -1,26 +1,26 @@
-function solution(s1, s2) {
-  var answer = "";
-  let list = [];
-  let pos = 0;
-
-  while (true) {
-    let position = s1.indexOf(s2, pos)
-    if (position === -1) break;
-    list.push(position);
-    pos = position + 1;
+function solution(s, t) {
+  let answer = [];
+  let p = 1000;
+  for (let x of s) {
+    if (x === t) {
+      p = 0;
+      answer.push(p);
+    }
+    else {
+      p++;
+      answer.push(p);
+    }
   }
-
-  for (let i = 0; i < list.length; i++) {
-
+  p = 1000;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === t) p = 0;
+    else {
+      p++;
+      answer[i] = Math.min(answer[i], p);
+    }
   }
-
   return answer;
 }
 
-const str1 = "teachermode"
-const str2 = "e"
-console.log(solution(str1, str2));
-// ▣ 입력예제 1
-// teachermode e
-// ▣ 출력예제 1
-// 1 0 1 2 1 0 1 2 2 1 0
+let str = "teachermode";
+console.log(solution(str, 'e'));
