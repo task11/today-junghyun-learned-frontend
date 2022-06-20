46bce7b57a58824e185125f6b75057f4035f86ce
task11 <task11@naver.com>
2022-06-20 23:58:14 +0900

    refactor: 브루트포스 문제 풀이(암호 만들기)

--- algorithm/baekjoon/brouteforce/1759.js
+++ algorithm/baekjoon/brouteforce/1759.js
@@ -1,13 +1,14 @@
-const fs = require('fs');
-const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n").map(v => v.split(' '));
-const [L, C] = input[0].map(Number);
-const sortedInput = input[1].sort();
+// const fs = require('fs');
+// const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n").map(v => v.split(' '));
+// const [L, C] = input[0].map(Number);
+// const sortedInput = input[1].sort();
+
 const output = [];
 const min = L - L * 2;
 
 function combinationRecursion(arr, data, s, idx, r) {
   if (s === r) {
-    if (isVaild(data)) {
+    if (isVowel(data)) {
       console.log(data.join(''));
     }
     return;
@@ -19,15 +20,15 @@ function combinationRecursion(arr, data, s, idx, r) {
   }
 }
 
-function isVaild(array) {
+function isVowel(array) {
   const value = ['a', 'e', 'i', 'o', 'u'];
-  let count = 0;
+  let vCount = 0;
+  let cCount = 0;
   array.forEach((item) => {
-    if (value.includes(item)) count++;
-    else count--;
+    if (value.includes(item)) vCount++;
+    else cCount++;
   });
-
-  return count >= 1 || count === min ? false : true;
+  return vCount >= 1 && cCount >= 2 ? true : false;
 }
 
 combinationRecursion(sortedInput, output, 0, 0, L); // Main Call
